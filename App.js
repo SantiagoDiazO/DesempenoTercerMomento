929363c2ef12ba0edc41762c46d189d5852cf10f
Santigo <santidiaz2112@gmail.com>
2023-06-05 16:38:49 -0500

    Primer commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeTabs from './components/HomeTabs';
+
+const Stack = createNativeStackNavigator()
+
+export default function App(){
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component={HomeTabs} options={{title: 'Sistema de almacen'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
